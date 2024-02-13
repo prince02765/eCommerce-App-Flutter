ff892935acce0482220e845a8334b9843d5afb2f
Prince Patel <prince90290@gmail.com>
2024-02-13 17:29:55 +0530

    Product details design completed

--- lib/features/shop/screens/product_details/product_details.dart
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -1,6 +1,11 @@
+import 'package:ecommerce_app/common/widgets/texts/section_heading.dart';
+import 'package:ecommerce_app/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart';
+import 'package:ecommerce_app/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:ecommerce_app/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:ecommerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
 
 import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/helpers/helper_functions.dart';
@@ -14,6 +19,7 @@ class ProductDetailsScreen extends StatelessWidget {
     final dark = THelperFunctions.isDarkMode(context);
 
     return Scaffold(
+      bottomNavigationBar: TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -33,9 +39,69 @@ class ProductDetailsScreen extends StatelessWidget {
 
                   // Price, title, stock & brand
                   TProductMetadata(),
+
+                  // Attributes
+                  TProductAttributes(),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
+                  // Checkout Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {}, child: Text("Checkout")),
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
+                  // Description
+                  TSectionHeading(
+                    title: "Description",
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwItems,
+                  ),
+                  ReadMoreText(
+                    "This is a product description for blue nike sleeve less vest. There are more things that can be added but I am just practicing and nothing else",
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: " Show More",
+                    trimExpandedText: " Less",
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
+                  // Reviews
+                  Divider(),
+                  const SizedBox(
+                    height: TSizes.spaceBtwItems,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TSectionHeading(
+                        title: "Review (199)",
+                        showActionButton: false,
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Iconsax.arrow_right_3,
+                            size: 18,
+                          ))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
---
+++ lib/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart
@@ -0,0 +1,68 @@
+import 'package:ecommerce_app/common/widgets/icons/circular_icon.dart';
+import 'package:ecommerce_app/utils/constants/colors.dart';
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../../utils/helpers/helper_functions.dart';
+
+class TBottomAddToCart extends StatelessWidget {
+  const TBottomAddToCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Container(
+      padding: EdgeInsets.symmetric(
+          horizontal: TSizes.defaultSpace, vertical: TSizes.defaultSpace / 2),
+      decoration: BoxDecoration(
+          color: dark ? TColors.darkGrey : TColors.light,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(TSizes.cardRadiusLg),
+              topRight: Radius.circular(TSizes.cardRadiusLg))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              TCircularIcon(
+                icon: Iconsax.minus,
+                backgroundColor: TColors.darkGrey,
+                width: 40,
+                height: 40,
+                color: dark ? TColors.white : TColors.black,
+              ),
+              SizedBox(
+                width: TSizes.spaceBtwItems,
+              ),
+              Text(
+                "2",
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              SizedBox(
+                width: TSizes.spaceBtwItems,
+              ),
+              TCircularIcon(
+                icon: Iconsax.add,
+                backgroundColor: TColors.black,
+                width: 40,
+                height: 40,
+                color: dark ? TColors.white : TColors.black,
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Add to Cart"),
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.all(TSizes.md),
+              backgroundColor: TColors.black,
+              side: BorderSide(color: TColors.black),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
